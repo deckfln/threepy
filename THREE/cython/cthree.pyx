4421cf6650735c0d5accc7677a99369c9f9b683a
fdeckert <flo.deckert@gmail.com>
2018-12-27 11:14:35 +0100

    try to help the compiler to vectorize more

--- THREE/cython/cthree.pyx
+++ THREE/cython/cthree.pyx
@@ -54,56 +54,91 @@ cpdef c_Matrix4_makeRotationFromQuaternion(np.ndarray[float, ndim=1] te , double
     te[15] = 1
 
 
-cpdef cMatrix4_multiplyMatrices(np.ndarray[float, ndim=1, mode='c'] te , np.ndarray[float, ndim=1, mode='c'] ae , np.ndarray[float, ndim=1, mode='c'] be ):
-        cdef float a11 = ae[0]
-        cdef float a12 = ae[4]
-        cdef float a13 = ae[8]
-        cdef float a14 = ae[12]
-        cdef float a21 = ae[1]
-        cdef float a22 = ae[5]
-        cdef float a23 = ae[9]
-        cdef float a24 = ae[13]
-        cdef float a31 = ae[2]
-        cdef float a32 = ae[6]
-        cdef float a33 = ae[10]
-        cdef float a34 = ae[14]
-        cdef float a41 = ae[3]
-        cdef float a42 = ae[7]
-        cdef float a43 = ae[11]
-        cdef float a44 = ae[15]
+cpdef cMatrix4_multiplyMatrices(np.ndarray[np.float32_t, ndim=1] te , np.ndarray[np.float32_t, ndim=1] ae , np.ndarray[np.float32_t, ndim=1] be ):
+        cdef np.float32_t a11
+        cdef np.float32_t a12
+        cdef np.float32_t a13
+        cdef np.float32_t a14
+        cdef np.float32_t a21
+        cdef np.float32_t a22
+        cdef np.float32_t a23
+        cdef np.float32_t a24
+        cdef np.float32_t a31
+        cdef np.float32_t a32
+        cdef np.float32_t a33
+        cdef np.float32_t a34
+        cdef np.float32_t a41
+        cdef np.float32_t a42
+        cdef np.float32_t a43
+        cdef np.float32_t a44
 
-        cdef float b11 = be[0]
-        cdef float b12 = be[4]
-        cdef float b13 = be[8]
-        cdef float b14 = be[12]
-        cdef float b21 = be[1]
-        cdef float b22 = be[5]
-        cdef float b23 = be[9]
-        cdef float b24 = be[13]
-        cdef float b31 = be[2]
-        cdef float b32 = be[6]
-        cdef float b33 = be[10]
-        cdef float b34 = be[14]
-        cdef float b41 = be[3]
-        cdef float b42 = be[7]
-        cdef float b43 = be[11]
-        cdef float b44 = be[15]
+        cdef np.float32_t b11
+        cdef np.float32_t b12
+        cdef np.float32_t b13
+        cdef np.float32_t b14
+        cdef np.float32_t b21
+        cdef np.float32_t b22
+        cdef np.float32_t b23
+        cdef np.float32_t b24
+        cdef np.float32_t b31
+        cdef np.float32_t b32
+        cdef np.float32_t b33
+        cdef np.float32_t b34
+        cdef np.float32_t b41
+        cdef np.float32_t b42
+        cdef np.float32_t b43
+        cdef np.float32_t b44
 
+        a11 = ae[0]
+        a12 = ae[4]
+        a13 = ae[8]
+        a14 = ae[12]
+        b11 = be[0]
+        b21 = be[1]
+        b31 = be[2]
+        b41 = be[3]
         te[0] = a11 * b11 + a12 * b21 + a13 * b31 + a14 * b41
+
+        b12 = be[4]
+        b22 = be[5]
+        b32 = be[6]
+        b42 = be[7]
         te[4] = a11 * b12 + a12 * b22 + a13 * b32 + a14 * b42
+
+        b13 = be[8]
+        b23 = be[9]
+        b33 = be[10]
+        b43 = be[11]
         te[8] = a11 * b13 + a12 * b23 + a13 * b33 + a14 * b43
+
+        b14 = be[12]
+        b24 = be[13]
+        b34 = be[14]
+        b44 = be[15]
         te[12] = a11 * b14 + a12 * b24 + a13 * b34 + a14 * b44
 
+        a21 = ae[1]
+        a22 = ae[5]
+        a23 = ae[9]
+        a24 = ae[13]
         te[1] = a21 * b11 + a22 * b21 + a23 * b31 + a24 * b41
         te[5] = a21 * b12 + a22 * b22 + a23 * b32 + a24 * b42
         te[9] = a21 * b13 + a22 * b23 + a23 * b33 + a24 * b43
         te[13] = a21 * b14 + a22 * b24 + a23 * b34 + a24 * b44
 
+        a31 = ae[2]
+        a32 = ae[6]
+        a33 = ae[10]
+        a34 = ae[14]
         te[2] = a31 * b11 + a32 * b21 + a33 * b31 + a34 * b41
         te[6] = a31 * b12 + a32 * b22 + a33 * b32 + a34 * b42
         te[10] = a31 * b13 + a32 * b23 + a33 * b33 + a34 * b43
         te[14] = a31 * b14 + a32 * b24 + a33 * b34 + a34 * b44
 
+        a41 = ae[3]
+        a42 = ae[7]
+        a43 = ae[11]
+        a44 = ae[15]
         te[3] = a41 * b11 + a42 * b21 + a43 * b31 + a44 * b41
         te[7] = a41 * b12 + a42 * b22 + a43 * b32 + a44 * b42
         te[11] = a41 * b13 + a42 * b23 + a43 * b33 + a44 * b43
